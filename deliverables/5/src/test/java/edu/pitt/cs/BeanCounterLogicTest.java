2b6a23145a6cabd28add4d3b5b7bf8789687cdef
wonsunahn <54322243+wonsunahn@users.noreply.github.com>
2024-07-25 03:00:12 -0400

    Update BeanCounterLogicTest.java

--- deliverables/5/src/test/java/edu/pitt/cs/BeanCounterLogicTest.java
+++ deliverables/5/src/test/java/edu/pitt/cs/BeanCounterLogicTest.java
@@ -21,7 +21,7 @@ import org.junit.runners.MethodSorters;
 import org.mockito.Mockito;
 
 /**
- * Code by @author Wonsun Ahn. Copyright Spring 2024.
+ * Code by @author Wonsun Ahn. Copyright Summer 2024.
  * 
  * <p>
  * Does integration testing on BeanCounterLogic. Makes tests deterministic by
